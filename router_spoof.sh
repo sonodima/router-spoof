328b14427a2bb79546512a9147a7adda4f8215ff
sonodima <62671493+sonodima@users.noreply.github.com>
2020-05-11 12:45:19 +0200

    Create router_spoof.sh

---
+++ router_spoof.sh
@@ -0,0 +1,43 @@
+#
+#  router_spoof.sh
+#
+#  ASH friendly script that spoofs mac addresses and changes network domain of OpenWRT based routers.
+#
+
+
+
+# Domain name changing
+
+# Generate random domain name
+GENERATED_DOMAIN="$(head /dev/urandom | tr -dc a-z | head -c 8).com"
+
+# Set the generated domain name
+echo "Setting domain: $GENERATED_DOMAIN"
+uci set dhcp.dnsmasq.domain=$GENERATED_DOMAIN
+
+# Apply changes
+echo "Applying dhcp changes"
+uci commit dhcp
+
+
+
+# Mac spoofing
+
+# Add interfaces you want to spoof to this list
+INTERFACES="eth0 eth1 eth2 eth3 eth5"
+
+# Iterate through each interface
+echo $INTERFACES | tr ' ' '\n' | while read INTERFACE; do
+    # Deactivate interface
+    echo "Deactivating $INTERFACE"
+    ifconfig $INTERFACE down
+
+    # Set new address to interface
+    NEW_MAC=$(hexdump -n3 -e'/3 "00:60:2F" 3/1 ":%02X"' /dev/random)
+    echo "Setting $NEW_MAC to $INTERFACE"
+    ifconfig $INTERFACE hw ether $NEW_MAC > /dev/null 2>&1
+
+    # Reactivate interface
+    echo "Activating $INTERFACE"
+    ifconfig $INTERFACE up
+done
